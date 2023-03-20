12c94dc3e33159f077a0cd92020a78f40e5fb953
Vignan Thumma <126392049+vignan8@users.noreply.github.com>
2023-03-20 20:43:00 +0530

    Delete link1.js

--- link1.js
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/prism.min.js"></script>
